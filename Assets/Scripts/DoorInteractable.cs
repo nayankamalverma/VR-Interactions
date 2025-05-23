8efdbfc9f9f442e309b4e98ea19d92d9642fd18f
nayankamalverma <nayankamalverma@gmail.com>
2025-05-23 14:33:18 +0530

    added limits on hinges

--- Assets/Scripts/DoorInteractable.cs
+++ Assets/Scripts/DoorInteractable.cs
@@ -3,18 +3,25 @@ using UnityEngine;
 
 public class DoorInteractable : SimpleHindgeInteractable
 {
-
     [SerializeField] private EventController eventController;
     [SerializeField] private Transform doorTransform;
     [SerializeField] private Vector3 rotationLimits;
-    private Transform startRotation;
+    [SerializeField] private Vector3 endRotation;
+    [SerializeField] private Collider doorCloseCollider;
+    [SerializeField] private Collider doorOpenCollider;
+
+    private bool isClosed;
+    private bool isOpen;
+    private Vector3 startRotation;
     private float startAngleX;
 
     protected override void Start()
     {
         base.Start();
-        startRotation = transform;
-        startAngleX = startRotation.localEulerAngles.x;
+        isClosed = true;
+        isOpen = false;
+        startRotation = transform.localEulerAngles;
+        startAngleX = startRotation.x;
         if (startAngleX >= 180)
         {
             startAngleX -= 360;
@@ -43,6 +50,8 @@ public class DoorInteractable : SimpleHindgeInteractable
 
     private void LimitCheck()
     {
+        isClosed = false;
+        isOpen = false;
         float localAngleX = transform.localEulerAngles.x;
 
         if (localAngleX >= 180)
@@ -53,6 +62,19 @@ public class DoorInteractable : SimpleHindgeInteractable
         if (localAngleX >= startAngleX + rotationLimits.x || localAngleX <= startAngleX - rotationLimits.x)
         {
             ReleaseHinge();
+        }
+    }
+    
+    protected override void ResetHinge()
+    {
+        if (isClosed)
+        {
+            transform.localEulerAngles = startRotation;
+        }
+        else if (isOpen) {
+            transform.localEulerAngles = endRotation;
+        }
+        {
             transform.localEulerAngles = new Vector3(startAngleX, transform.localEulerAngles.y,transform.localEulerAngles.z);
         }
     }
@@ -64,4 +86,17 @@ public class DoorInteractable : SimpleHindgeInteractable
         eventController.DoorLockAction -= OnDoorLock;
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other == doorCloseCollider)
+        {
+            isClosed = true;
+            ReleaseHinge();
+        }
+        else if (other == doorOpenCollider)
+        {
+            isOpen = true;
+            ReleaseHinge();
+        }
+    }
 }
